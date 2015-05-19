a0944a6927deee56374f245a92122080f7a50959
Elliott Draper <el@kickcode.com>
2015-05-19 11:27:52 +0100

    basic file drag handling, displaying the image if it's an image file, or a file icon if not

--- app/drag_and_drop_view.rb
+++ app/drag_and_drop_view.rb
@@ -1,2 +1,45 @@
 class DragAndDropView < NSImageView
+  def draggingEntered(info)
+    self.highlight!
+    NSDragOperationCopy
+  end
+
+  def draggingExited(info)
+    self.unhighlight!
+  end
+
+  def prepareForDragOperation(info)
+    self.unhighlight!
+  end
+
+  def performDragOperation(info)
+    if info.draggingSource != self
+      if NSImage.canInitWithPasteboard(info.draggingPasteboard)
+        image = NSImage.alloc.initWithPasteboard(info.draggingPasteboard)
+      else
+        image = NSImage.imageNamed("file_icon")
+      end
+      self.setImage(image)
+    end
+  end
+
+  def highlight!
+    @highlight = true
+    self.needsDisplay = true
+  end
+
+  def unhighlight!
+    @highlight = false
+    self.needsDisplay = true
+  end
+
+  def drawRect(frame)
+    super(frame)
+
+    if @highlight
+      NSColor.grayColor.set
+      NSBezierPath.setDefaultLineWidth(5)
+      NSBezierPath.strokeRect(frame)
+    end
+  end
 end
